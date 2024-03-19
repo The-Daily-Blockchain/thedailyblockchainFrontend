d4fe55362ac19a865e5d6fb8dfe676e4abbd50e2
Dexter <dexter.lente18@gmail.com>
2024-03-19 15:06:37 +0800

    separating lightweight chart creating logic

---
+++ src/app/(tabs)/cryptocurrency/chartComponent.tsx
@@ -0,0 +1,132 @@
+import { useEffect, useRef, useState } from "react";
+import {
+  createChart,
+  IChartApi,
+  ISeriesApi,
+  PriceScaleMode,
+} from "lightweight-charts";
+import { Tooltip } from "./tooltip";
+
+interface ChartProps {
+  formattedData: any[] | null;
+  loading: boolean;
+}
+
+export const ChartComponent = ({ formattedData, loading }: ChartProps) => {
+  const chartContainerRef = useRef<HTMLDivElement>(null);
+  const chartRef = useRef<IChartApi | null>(null);
+  const seriesRef = useRef<ISeriesApi<"Area"> | null>(null);
+  const secondSeries = useRef<ISeriesApi<"Histogram"> | null>(null);
+  const [tooltipData, setTooltipData] = useState<any>(null);
+  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    if (!formattedData || !chartContainerRef.current || loading) return;
+
+    const chart =
+      chartRef.current ??
+      createChart(chartContainerRef.current, {
+        width: 600,
+        height: 300,
+        autoSize: true,
+        rightPriceScale: {
+          mode: PriceScaleMode.Normal,
+          autoScale: false,
+          invertScale: false,
+          alignLabels: false,
+          scaleMargins: {
+            top: 0.3,
+            bottom: 0,
+          },
+        },
+      });
+    chartRef.current = chart;
+
+    const lineSeries =
+      seriesRef.current ??
+      chart.addAreaSeries({
+        topColor: "#5bb450",
+        bottomColor: "#ffffe0",
+        lineColor: "#123524",
+        lineWidth: 1,
+        crosshairMarkerVisible: false,
+      });
+    lineSeries.priceScale().applyOptions({
+      scaleMargins: {
+        top: 0.1,
+        bottom: 0.4,
+      },
+    });
+    lineSeries.setData(
+      formattedData.map((entry) => ({
+        time: entry.time,
+        value: entry.price,
+      }))
+    );
+    seriesRef.current = lineSeries;
+
+    const barSeries = chart.addHistogramSeries({
+      color: "#5A5A5A",
+      priceFormat: {
+        type: "volume",
+      },
+      priceScaleId: "",
+    });
+    secondSeries.current = barSeries;
+    barSeries.priceScale().applyOptions({
+      scaleMargins: {
+        top: 0.7,
+        bottom: 0,
+      },
+    });
+
+    const formatVolume = formattedData.map((entry) => ({
+      time: entry.time,
+      value: entry.volume,
+    }));
+    const colorData = formatVolume.map((item, index) => {
+      if (index === 0) return { ...item, color: "#5bb450" };
+      const prevValue = formatVolume[index - 1].value;
+      const color = item.value - prevValue >= 0 ? "#5bb450" : "#ff0000";
+      return { ...item, color };
+    });
+    barSeries.setData(colorData);
+
+    const updateToolTip = (param: any) => {
+      setTooltipData(param);
+      if (param.point) {
+        const x = param.point.x;
+        const y = param.point.y;
+        setTooltipPosition({ x, y });
+      }
+    };
+    chartRef.current.subscribeCrosshairMove(updateToolTip);
+    chartRef.current.timeScale().fitContent();
+
+    return () => {
+      if (chartRef.current) {
+        chartRef.current.unsubscribeCrosshairMove(updateToolTip);
+      }
+      setTooltipData(null);
+    };
+  }, [formattedData, loading]);
+
+  return (
+    <div className="chart-container relative">
+      <div
+        className="chart border-2 border-solid"
+        ref={chartContainerRef}
+      ></div>
+      <div className="legend"></div>
+      {tooltipData?.time && seriesRef.current && secondSeries.current && (
+        <Tooltip
+          time={tooltipData?.time}
+          value={tooltipData?.seriesData?.get(seriesRef.current)?.value}
+          volume={tooltipData?.seriesData?.get(secondSeries.current)?.value}
+          x={tooltipData?.point?.x}
+          y={tooltipData?.point?.y}
+        />
+      )}
+    </div>
+  );
+};
--- src/app/(tabs)/cryptocurrency/charting.tsx
+++ src/app/(tabs)/cryptocurrency/charting.tsx
@@ -5,7 +5,7 @@ import {
   ISeriesApi,
   PriceScaleMode,
 } from "lightweight-charts";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Tooltip } from "./tooltip";
 
 import {
@@ -13,18 +13,13 @@ import {
   formatDate,
 } from "@/app/_components/utils/formattingData";
 import { useChartData } from "@/app/_components/hooks/useChartData";
+import { ChartComponent } from "./chartComponent";
 
 interface Props {
   symbol: any;
 }
 
 const Charting = ({ symbol }: Props) => {
-  const chartContainerRef = useRef(null);
-  const chartRef = useRef<IChartApi | null>(null);
-  const seriesRef = useRef<ISeriesApi<"Area"> | null>(null);
-  const secondSeries = useRef<ISeriesApi<"Histogram"> | null>(null);
-  const [tooltipData, setTooltipData] = useState<any>(null);
-  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
   const [formattedData, setFormattedData] = useState<any[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
@@ -50,124 +45,9 @@ const Charting = ({ symbol }: Props) => {
     }
   }, [chartData]);
 
-  useEffect(() => {
-    if (!formattedData || !chartContainerRef.current) return;
-    if (!loading) {
-      const chart =
-        chartRef.current ??
-        createChart(chartContainerRef.current, {
-          width: 600,
-          height: 300,
-          autoSize: true,
-          rightPriceScale: {
-            mode: PriceScaleMode.Normal,
-            autoScale: false,
-            invertScale: false,
-            alignLabels: false,
-            scaleMargins: {
-              top: 0.3,
-              bottom: 0,
-            },
-          },
-        });
-      chartRef.current = chart;
-
-      const lineSeries =
-        seriesRef.current ??
-        chart.addAreaSeries({
-          topColor: "#5bb450",
-          bottomColor: "#ffffe0",
-          lineColor: "#123524",
-          lineWidth: 1,
-          crosshairMarkerVisible: false,
-        });
-      lineSeries.priceScale().applyOptions({
-        scaleMargins: {
-          top: 0.1, // highest point of the series will be 10% away from the top
-          bottom: 0.4, // lowest point will be 40% away from the bottom
-        },
-      });
-
-      // lineSeries.setData(formatPrice);
-      lineSeries.setData(
-        formattedData?.map((entry) => ({
-          time: entry.time,
-          value: entry.price,
-        }))
-      );
-
-      seriesRef.current = lineSeries;
-
-      const barSeries = chart.addHistogramSeries({
-        color: "#5A5A5A",
-        priceFormat: {
-          type: "volume",
-        },
-        priceScaleId: "",
-      });
-
-      secondSeries.current = barSeries;
-
-      barSeries.priceScale().applyOptions({
-        // set the positioning of the volume series
-        scaleMargins: {
-          top: 0.7, // highest point of the series will be 70% away from the top
-          bottom: 0,
-        },
-      });
-
-      const formatVolume = formattedData?.map((entry) => ({
-        time: entry.time,
-        value: entry.volume,
-      }));
-
-      const colorData = formatVolume.map((item, index) => {
-        if (index === 0) return { ...item, color: "#5bb450" }; // Initial color
-        const prevValue = formatVolume[index - 1].value;
-        const color = item.value - prevValue >= 0 ? "#5bb450" : "#ff0000"; // Green for positive, red for negative
-        return { ...item, color };
-      });
-      barSeries.setData(colorData);
-
-      const updateToolTip = (param: any) => {
-        setTooltipData(param);
-        if (param.point) {
-          const x = param.point.x;
-          const y = param.point.y;
-          setTooltipPosition({ x, y });
-        }
-      };
-
-      chartRef.current.subscribeCrosshairMove(updateToolTip);
-      chartRef.current.timeScale().fitContent();
-
-      return () => {
-        if (chartRef.current) {
-          chartRef.current.unsubscribeCrosshairMove(updateToolTip);
-        }
-        setTooltipData(null);
-      };
-    }
-  }, [chartData, formattedData, loading]);
-
   return (
     <>
-      <div className="chart-container relative">
-        <div
-          className="chart border-2 border-solid"
-          ref={chartContainerRef}
-        ></div>
-        <div className="legend"></div>
-        {tooltipData?.time && seriesRef.current && secondSeries.current && (
-          <Tooltip
-            time={tooltipData?.time}
-            value={tooltipData?.seriesData?.get(seriesRef.current)?.value}
-            volume={tooltipData?.seriesData?.get(secondSeries.current)?.value}
-            x={tooltipData?.point?.x}
-            y={tooltipData?.point?.y}
-          />
-        )}
-      </div>
+      <ChartComponent formattedData={formattedData} loading={loading} />
     </>
   );
 };
