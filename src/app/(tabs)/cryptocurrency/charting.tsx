b371e8024128b73496d66e81d0fe05633febe041
Dexter <dexter.lente18@gmail.com>
2024-03-19 13:57:59 +0800

    chart fixed

--- src/app/(tabs)/cryptocurrency/charting.tsx
+++ src/app/(tabs)/cryptocurrency/charting.tsx
@@ -32,132 +32,123 @@ const Charting = ({ symbol }: Props) => {
   const params = symbol.toUpperCase();
   const startTime = currentDate.getTime() - 7 * 24 * 60 * 60 * 1000;
   const interval = "1d";
+
   const { data: chartData } = useChartData(params, startTime, interval);
-  // console.log(chartData);
-  // useEffect(() => {
-  //   if (Array.isArray(chartData)) {
-  //     const formatted = chartData.map((item: string[]) => ({
-  //       time: formatChartingDate(item[0]),
-  //       price: parseFloat(item[1]),
-  //       volume: parseFloat(item[5]),
-  //     }));
-  //     console.log(formatted);
-  //     setFormattedData(formatted);
-  //     setLoading(false);
-  //   } else {
-  //     setFormattedData(null);
-  //     setLoading(false);
-  //   }
-  // }, [chartData]);
 
-  // console.log(formattedData);
   useEffect(() => {
-    if (!chartContainerRef.current) return;
+    if (chartData && Array.isArray(chartData)) {
+      const formatted = chartData.map((item: string[]) => ({
+        time: formatChartingDate(item[0]),
+        price: parseFloat(item[1]),
+        volume: parseFloat(item[5]),
+      }));
+      setFormattedData(formatted);
+      setLoading(false);
+    } else {
+      setFormattedData(null);
+      setLoading(false);
+    }
+  }, [chartData]);
 
-    const chart = createChart(chartContainerRef.current, {
-      width: 600,
-      height: 300,
-      autoSize: true,
-      rightPriceScale: {
-        mode: PriceScaleMode.Normal,
-        autoScale: false,
-        invertScale: false,
-        alignLabels: false,
+  useEffect(() => {
+    if (!formattedData || !chartContainerRef.current) return;
+    if (!loading) {
+      const chart =
+        chartRef.current ??
+        createChart(chartContainerRef.current, {
+          width: 600,
+          height: 300,
+          autoSize: true,
+          rightPriceScale: {
+            mode: PriceScaleMode.Normal,
+            autoScale: false,
+            invertScale: false,
+            alignLabels: false,
+            scaleMargins: {
+              top: 0.3,
+              bottom: 0,
+            },
+          },
+        });
+      chartRef.current = chart;
+
+      const lineSeries =
+        seriesRef.current ??
+        chart.addAreaSeries({
+          topColor: "#5bb450",
+          bottomColor: "#ffffe0",
+          lineColor: "#123524",
+          lineWidth: 1,
+          crosshairMarkerVisible: false,
+        });
+      lineSeries.priceScale().applyOptions({
         scaleMargins: {
-          top: 0.3,
+          top: 0.1, // highest point of the series will be 10% away from the top
+          bottom: 0.4, // lowest point will be 40% away from the bottom
+        },
+      });
+
+      // lineSeries.setData(formatPrice);
+      lineSeries.setData(
+        formattedData?.map((entry) => ({
+          time: entry.time,
+          value: entry.price,
+        }))
+      );
+
+      seriesRef.current = lineSeries;
+
+      const barSeries = chart.addHistogramSeries({
+        color: "#5A5A5A",
+        priceFormat: {
+          type: "volume",
+        },
+        priceScaleId: "",
+      });
+
+      secondSeries.current = barSeries;
+
+      barSeries.priceScale().applyOptions({
+        // set the positioning of the volume series
+        scaleMargins: {
+          top: 0.7, // highest point of the series will be 70% away from the top
           bottom: 0,
         },
-      },
-    });
-    chartRef.current = chart;
+      });
 
-    const lineSeries = chart.addAreaSeries({
-      topColor: "#5bb450",
-      bottomColor: "#ffffe0",
-      lineColor: "#123524",
-      lineWidth: 1,
-      crosshairMarkerVisible: false,
-    });
+      const formatVolume = formattedData?.map((entry) => ({
+        time: entry.time,
+        value: entry.volume,
+      }));
 
-    lineSeries.priceScale().applyOptions({
-      scaleMargins: {
-        top: 0.1, // highest point of the series will be 10% away from the top
-        bottom: 0.4, // lowest point will be 40% away from the bottom
-      },
-    });
+      const colorData = formatVolume.map((item, index) => {
+        if (index === 0) return { ...item, color: "#000000" }; // Initial color
+        const prevValue = formatVolume[index - 1].value;
+        const color = item.value - prevValue >= 0 ? "#5bb450" : "#ff0000"; // Green for positive, red for negative
+        return { ...item, color };
+      });
+      barSeries.setData(colorData);
 
-    // lineSeries.setData(formatPrice);
-    const formatPrice = chartData?.map((entry: any) => ({
-      time: entry.time,
-      value: entry.price,
-    }));
-    console.log("formatPrice:", formatPrice);
+      const updateToolTip = (param: any) => {
+        setTooltipData(param);
+        if (param.point) {
+          const x = param.point.x;
+          const y = param.point.y;
+          setTooltipPosition({ x, y });
+        }
+      };
 
-    const data = [
-      { time: "2024-03-18", value: 25 },
-      { time: "2024-03-19", value: 30 },
-      { time: "2024-03-20", value: 28 },
-      { time: "2024-03-21", value: 32 },
-      { time: "2024-03-22", value: 35 },
-      { time: "2024-03-23", value: 40 },
-      { time: "2024-03-24", value: 38 },
-      { time: "2024-03-25", value: 42 },
-    ];
+      chartRef.current.subscribeCrosshairMove(updateToolTip);
+      chartRef.current.timeScale().fitContent();
 
-    lineSeries.setData(data);
-
-    seriesRef.current = lineSeries;
-
-    const barSeries = chart.addHistogramSeries({
-      color: "#5A5A5A",
-      priceFormat: {
-        type: "volume",
-      },
-      priceScaleId: "",
-    });
-
-    secondSeries.current = barSeries;
-
-    barSeries.priceScale().applyOptions({
-      // set the positioning of the volume series
-      scaleMargins: {
-        top: 0.7, // highest point of the series will be 70% away from the top
-        bottom: 0,
-      },
-    });
-
-    const formatVolume = formattedData?.map((entry) => ({
-      time: entry.time,
-      value: entry.volume,
-    }));
-
-    const colorData = data.map((item, index) => {
-      if (index === 0) return { ...item, color: "#000000" }; // Initial color
-      const prevValue = data[index - 1].value;
-      const color = item.value - prevValue >= 0 ? "#5bb450" : "#ff0000"; // Green for positive, red for negative
-      return { ...item, color };
-    });
-    barSeries.setData(colorData);
-
-    const updateToolTip = (param: any) => {
-      setTooltipData(param);
-      if (param.point) {
-        const x = param.point.x;
-        const y = param.point.y;
-        setTooltipPosition({ x, y });
-      }
-    };
-
-    chartRef.current.subscribeCrosshairMove(updateToolTip);
-    chartRef.current.timeScale().fitContent();
-
-    return () => {
-      if (chartRef.current) {
-        chartRef.current.unsubscribeCrosshairMove(updateToolTip);
-      }
-      setTooltipData(null);
-    };
-  }, [chartData]);
+      return () => {
+        if (chartRef.current) {
+          chartRef.current.unsubscribeCrosshairMove(updateToolTip);
+        }
+        setTooltipData(null);
+      };
+    }
+  }, [chartData, formattedData, loading]);
 
   return (
     <>
--- src/app/_components/hooks/useChartData.ts
+++ src/app/_components/hooks/useChartData.ts
@@ -1,25 +1,31 @@
 import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { useDebouncedValue } from "../utils/usedebouncevalue";
+import useCachedData from "./usecacheddata";
 import useCachedDynamicData from "./usecacheddynamicdata";
-import { formatChartingDate } from "../utils/formattingData";
 
 export const useChartData = (symbol: any, startTime: any, interval: any) => {
   const urls = `/api/graph?symbol=${symbol}&startTime=${startTime}&interval=${interval}`;
 
-  const cacheOptions = {
-    key: "chart_data_of_",
-    expirationTime: 8640000,
-  };
+  const debounceUrls = useDebouncedValue(urls, 8640000);
 
-  const data = useCachedDynamicData(urls, cacheOptions, symbol);
-  const newData = useRef<any>(null);
+  const [newData, setNewData] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
-    if (data) {
-      newData.current = data;
-    }
-  }, [data]);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(debounceUrls);
+        const data = await response.data;
+        setNewData(data);
+      } catch (error) {
+        console.error("Error fetching chart data:", error);
+        setNewData(null);
+      }
+    };
+    fetchData();
+  }, [debounceUrls, symbol]);
+  console.log(newData);
 
-  console.log(newData.current);
-
-  return { data: newData.current };
+  return { data: newData };
 };
