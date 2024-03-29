4c0c82242f18b55e4146237f1b09c69c3f2d4290
Dexterlente <dexter.lente18@gmail.com>
2024-03-30 00:01:17 +0800

    added page

--- src/app/(tabs)/cryptocurrency/cryptopost/cryptoPost.tsx
+++ src/app/(tabs)/cryptocurrency/cryptopost/cryptoPost.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useGetCryptoPost } from "@/app/_components/hooks/useGetCryptoPost";
 import React from "react";
+import parse from "html-react-parser";
 
 interface Props {
   symbol: any;
@@ -9,7 +10,20 @@ interface Props {
 const CryptoPost = ({ symbol }: Props) => {
   const { data, isLoading } = useGetCryptoPost(symbol);
   console.log(data);
-  return <div>loremisum ipsum</div>;
+
+  return (
+    <>
+      <div>What is on yourmind</div>
+      {data?.results?.map((items: any) => (
+        <div
+          className="border-b-2 border-solid border-gray-300 mb-2 pb-2"
+          key={items.id}
+        >
+          {parse(items.description)}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default CryptoPost;
