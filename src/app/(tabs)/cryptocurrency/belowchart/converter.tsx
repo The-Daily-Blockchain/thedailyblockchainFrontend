6ed0b9ee59de8ec3d69b174af3580bf2c6b3c30e
Dexter <dexter.lente18@gmail.com>
2024-04-01 18:56:57 +0800

    converter

--- src/app/(tabs)/cryptocurrency/belowchart/converter.tsx
+++ src/app/(tabs)/cryptocurrency/belowchart/converter.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { capitalizeFirstLetter } from "@/app/_components/utils/capitalizefirstletter";
 import React from "react";
 
 interface Props {
@@ -8,7 +9,19 @@ interface Props {
 const Converter = ({ symbol }: Props) => {
   return (
     <div>
-      <div>{symbol} to US Dollar converter</div>
+      <div className="font-semibold pt-3 mb-1 ml-3">
+        {capitalizeFirstLetter(symbol)} to US Dollar converter
+      </div>
+      <div className="flex justify-between px-3 pb-3">
+        <input
+          className="border-2 justify-center border-black border-solid rounded-xl"
+          style={{ width: "150px" }}
+        ></input>
+        <input
+          className="border-2 border-black border-solid rounded-xl"
+          style={{ width: "150px" }}
+        ></input>
+      </div>
     </div>
   );
 };
