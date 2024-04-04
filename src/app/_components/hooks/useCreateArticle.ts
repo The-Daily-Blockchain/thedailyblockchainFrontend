9a6e64b150b382da33f6d593c2304dd080ee2ecc
Dexter <dexter.lente18@gmail.com>
2024-04-04 14:51:47 +0800

    addedusecreatearticlehook

---
+++ src/app/_components/hooks/useCreateArticle.ts
@@ -0,0 +1,22 @@
+import useSWR from "swr";
+import { fetcher } from "../utils/fetcher";
+import { POST } from "@/app/api/article/route";
+
+export const usePostWithToken = () => {
+  // pass the body here
+  const token = "f7a241925df6abaecf7a7b8a408a41d6f9119b50";
+  const url = `/api/article`;
+  const { error, mutate } = useSWR(url, fetcher);
+
+  const postData = async (body: any) => {
+    try {
+      const response = await POST(body, token);
+      const data = await response.json();
+      mutate(data);
+    } catch (error) {
+      console.error("Error making POST request:", error);
+    }
+  };
+
+  return { error, postData };
+};
