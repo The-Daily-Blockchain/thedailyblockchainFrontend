72706905b3e8536bd41d7d777394e0bafa1ae8a3
Dexter <dexter.lente18@gmail.com>
2024-04-04 14:25:53 +0800

    making serverside

--- src/app/api/article/route.ts
+++ src/app/api/article/route.ts
@@ -1,7 +1,7 @@
 import { API_URL } from "@/app/config";
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextApiResponse) {
   let url = `${API_URL}/articles`;
@@ -13,5 +13,20 @@ export async function GET(req: NextRequest, res: NextApiResponse) {
 
   const response = await axios.get(url);
   const data = response.data;
+  return Response.json(data);
+}
+
+export async function POST(req: NextRequest, token: string) {
+  let url = `${API_URL}/articles/`;
+  // const token = "f7a241925df6abaecf7a7b8a408a41d6f9119b50";
+  const body = await req.json();
+  const response = await axios.post(url, body, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+  });
+  const data = await response.data;
+
   return Response.json(data);
 }
