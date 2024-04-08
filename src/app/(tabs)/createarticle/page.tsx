da0f8c5cdcb38dab4f11a9df71db3626014fa6b5
Dexter <dexter.lente18@gmail.com>
2024-04-08 18:28:02 +0800

    get returned data

--- src/app/(tabs)/createarticle/page.tsx
+++ src/app/(tabs)/createarticle/page.tsx
@@ -6,6 +6,7 @@ const Page = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [image, setImage] = useState(null);
+  const [responseData, setResponseData] = useState(null);
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
@@ -18,7 +19,7 @@ const Page = () => {
     try {
       const response = await axios.post(`api/article/`, formData);
       const data = response.data;
-      console.log(data);
+      setResponseData(data);
     } catch (error) {
       console.error("Error:", error);
     }
@@ -29,28 +30,40 @@ const Page = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Title:</label>
-      <br />
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <br />
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Title:</label>
+        <br />
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <br />
 
-      <label>Content:</label>
-      <br />
-      <textarea value={content} onChange={(e) => setContent(e.target.value)} />
-      <br />
+        <label>Content:</label>
+        <br />
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <br />
 
-      <label>Image:</label>
-      <br />
-      <input type="file" onChange={handleFileChange} />
-      <br />
+        <label>Image:</label>
+        <br />
+        <input type="file" onChange={handleFileChange} />
+        <br />
 
-      <button type="submit">Submit</button>
-    </form>
+        <button type="submit">Submit</button>
+      </form>
+
+      {responseData && (
+        <div>
+          <h2>Returned Data</h2>
+          <pre>{JSON.stringify(responseData, null, 2)}</pre>
+        </div>
+      )}
+    </div>
   );
 };
 
