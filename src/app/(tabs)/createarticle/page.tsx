bf5c16d1db04bb290ccb17a6a15502e36845b8fb
Dexter <dexter.lente18@gmail.com>
2024-04-08 18:13:19 +0800

    page created

--- src/app/(tabs)/createarticle/page.tsx
+++ src/app/(tabs)/createarticle/page.tsx
@@ -1,8 +1,57 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-const page = () => {
-  return <div>page</div>;
+const Page = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [image, setImage] = useState(null);
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("content", content);
+    formData.append("image", image as any);
+
+    try {
+      const response = await axios.post(`api/article/`, formData);
+      const data = response.data;
+      console.log(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleFileChange = (event: any) => {
+    setImage(event.target.files[0]);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Title:</label>
+      <br />
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <br />
+
+      <label>Content:</label>
+      <br />
+      <textarea value={content} onChange={(e) => setContent(e.target.value)} />
+      <br />
+
+      <label>Image:</label>
+      <br />
+      <input type="file" onChange={handleFileChange} />
+      <br />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
-export default page;
+export default Page;
--- src/app/_components/hooks/useCreateArticle.ts
+++ src/app/_components/hooks/useCreateArticle.ts
@@ -2,15 +2,15 @@ import useSWR from "swr";
 import { fetcher } from "../utils/fetcher";
 import { POST } from "@/app/api/article/route";
 
-export const usePostWithToken = () => {
+export const usePostWithToken = (formData: any) => {
   // pass the body here
   const token = "f7a241925df6abaecf7a7b8a408a41d6f9119b50";
   const url = `/api/article`;
   const { error, mutate } = useSWR(url, fetcher);
 
-  const postData = async (body: any) => {
+  const postData = async (formData: any) => {
     try {
-      const response = await POST(body, token);
+      const response = await POST(formData, token);
       const data = await response.json();
       mutate(data);
     } catch (error) {
