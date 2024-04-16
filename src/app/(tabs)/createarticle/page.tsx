fcf42edd928d4305d6223f449195a9a37b27b3b9
Dexter <dexter.lente18@gmail.com>
2024-04-16 18:01:52 +0800

    fix editor

--- src/app/(tabs)/createarticle/page.tsx
+++ src/app/(tabs)/createarticle/page.tsx
@@ -92,6 +92,7 @@ const Page = () => {
           <Editor
             apiKey={apiKey}
             init={{
+              branding: false,
               plugins:
                 "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate mentions tableofcontents footnotes mergetags autocorrect typography inlinecss markdown",
               toolbar:
