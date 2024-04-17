0e785c562e5b41ba08dede563cb195e838322c88
Dexter <dexter.lente18@gmail.com>
2024-04-17 14:35:58 +0800

    addwards nodonyet button

--- src/app/_components/component/logout.tsx
+++ src/app/_components/component/logout.tsx
@@ -4,9 +4,11 @@ import { Button } from "@/components/ui/button";
 import axios from "axios";
 import Cookies from "js-cookie";
 import { useGetLogUser } from "../hooks/useGetLogUser";
+import { useRouter } from "next/navigation";
 
 const LogoutButton = () => {
   const { isLoggedIn, logout } = useAuth();
+  const router = useRouter();
   const handleLogout = async () => {
     const response = await axios.post(`api/logout`);
     Cookies.remove("token");
@@ -16,15 +18,32 @@ const LogoutButton = () => {
   const { data } = useGetLogUser();
   return (
     isLoggedIn && (
-      <div className="flex py-2 mr-2 items-center">
-        {data && (
-          <div className="flex-1 ml-2 text-left">
-            Logged User: {data?.first_name} {data?.last_name}
+      <div>
+        <div className="flex py-2 mr-2 items-center">
+          {data && (
+            <div className="flex-1 ml-2 text-left">
+              Logged User: {data?.first_name} {data?.last_name}
+            </div>
+          )}
+          <div>
+            <Button className="flex-1 hover:opacity-60" onClick={handleLogout}>
+              Logout
+            </Button>
           </div>
-        )}
-        <div>
-          <Button className="flex-1" onClick={handleLogout}>
-            Logout
+        </div>
+        <div className="flex">
+          <Button
+            // variant
+            onClick={() => router.push("/createarticle")}
+            className="ml-2 mb-2 hover:opacity-60"
+          >
+            Create Article
+          </Button>
+          <Button
+            onClick={() => router.push("/createpost")}
+            className="ml-2 mb-2 hover:opacity-60"
+          >
+            Create Post
           </Button>
         </div>
       </div>
