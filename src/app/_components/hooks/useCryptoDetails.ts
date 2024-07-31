064633bdb584de8824a9933d58f7e892405d513d
Dexter <dexter.lente18@gmail.com>
2024-07-31 17:08:04 +0800

    fix useeffect

--- src/app/_components/hooks/useCryptoDetails.ts
+++ src/app/_components/hooks/useCryptoDetails.ts
@@ -1,8 +1,17 @@
 import useSWR from "swr";
 import { fetcher } from "../utils/fetcher";
+import { useEffect, useRef } from "react";
 
 export const useCryptoDetails = (symbol: string) => {
   const urls = `/api/cryptodetails/details/${symbol}`;
   const { data, isLoading } = useSWR(urls, fetcher);
+
+  const cachedDataRef = useRef<any>(null);
+
+  useEffect(() => {
+    if (data) {
+      cachedDataRef.current = data;
+    }
+  }, [data, symbol]);
   return { data, isLoading };
 };
--- src/app/_components/hooks/useGetCryptoPost.ts
+++ src/app/_components/hooks/useGetCryptoPost.ts
@@ -1,9 +1,18 @@
 import useSWR from "swr";
 import { fetcher } from "../utils/fetcher";
+import { useEffect, useRef } from "react";
 
 export const useGetCryptoPost = (symbol: any, page: number) => {
   const url = `/api/cryptopost/details/${symbol}/${page}`;
   const { data, isLoading } = useSWR(url, fetcher);
+
+  const cachedDataRef = useRef<any>(null);
+
+  useEffect(() => {
+    if (data) {
+      cachedDataRef.current = data;
+    }
+  }, [data, symbol]);
 
   return { data, isLoading };
 };
