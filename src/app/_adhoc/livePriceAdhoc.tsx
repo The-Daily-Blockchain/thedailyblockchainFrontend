6001c94e64d99e3e235653587e5673be4296e89d
Dexter <dexter.lente18@gmail.com>
2024-08-09 16:57:03 +0800

    fix

--- src/app/_adhoc/livePriceAdhoc.tsx
+++ src/app/_adhoc/livePriceAdhoc.tsx
@@ -7,7 +7,7 @@ interface Props {
 const LivePriceAdhoc = ({ children }: Props) => {
   return (
     <div
-      className="grid mx-2 xl:grid-cols-[150px_auto_150px]  2xl:grid-cols-[340px_auto_340px]"
+      className="xl:grid mx-2 xl:grid-cols-[150px_auto_150px]  2xl:grid-cols-[340px_auto_340px]"
       style={{
         overflowX: "auto",
         scrollbarColor: "transparent transparent",
