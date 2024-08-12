0abd0177b20098b9fda2c364a7206dc6fb684c24
Dexter <dexter.lente18@gmail.com>
2024-08-12 10:22:34 +0800

    update

--- next.config.js
+++ next.config.js
@@ -2,12 +2,41 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: [
-      "static01.nyt.com",
-      "images.gmanews.tv",
-      "www.nytimes.com",
-      "res.cloudinary.com",
-    ],
+    domains: ["res.cloudinary.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; img-src 'self' https: data:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; object-src 'none';",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(self), microphone=()",
+          },
+        ],
+      },
+    ];
   },
 };
 
