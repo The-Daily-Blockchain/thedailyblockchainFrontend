dbdc44047f83bfcfe3e9ca91e7f5cf0e3628e967
Dexter <dexter.lente18@gmail.com>
2024-08-17 14:13:45 +0800

    add suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import "./globals.css";
 import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
+import { Suspense } from "react";
 import NavBar from "@/app/_navbar/navbar";
 import Footer from "./_navbar/footer";
 import { AuthProvider, useAuth } from "./_context/authContext";
 import LogoutButton from "./_components/component/logout";
 import { ROOT_SEO } from "@/app/_seo/seo_config";
+import Loading from "./_navbar/loading";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -23,16 +25,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <AuthProvider>
-          <NavBar />
-          <div className="bg-black text-white">
-            <LogoutButton />
-          </div>
-          {children}
-          <Footer />
-        </AuthProvider>
-      </body>
+      <Suspense fallback={<Loading />}>
+        <body>
+          <AuthProvider>
+            <NavBar />
+            <div className="bg-black text-white">
+              <LogoutButton />
+            </div>
+            {children}
+            <Footer />
+          </AuthProvider>
+        </body>
+      </Suspense>
     </html>
   );
 }
