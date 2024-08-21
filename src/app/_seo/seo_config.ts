18ade56011087a63840fb3b735c72ed5d33a1828
Dexter <dexter.lente18@gmail.com>
2024-08-21 10:40:23 +0800

    added fav

--- src/app/_seo/seo_config.ts
+++ src/app/_seo/seo_config.ts
@@ -3,7 +3,7 @@ export const ROOT_SEO = {
     "The Daily Blockchain PH: Your Ultimate Source for Cryptocurrency News and Insights",
   DESCRIPTION:
     "Giving you the daily dose of cryptocurrency news and insights, The Daily Blockchain PH keeps you informed about blockchain technology, market trends, and digital currencies. From expert analysis to the latest updates, we provide comprehensive coverage to help you navigate the crypto world with confidence, whether you're an experienced investor or just starting out.",
-  LOGO: "/LOGO.png",
+  LOGO: "/favicon.ico",
   AUTHOR: "Dexter Lente",
 };
 
