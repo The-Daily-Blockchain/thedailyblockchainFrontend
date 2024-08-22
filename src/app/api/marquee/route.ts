792579468ce2332ff55cb6180f2ed378790e6a73
Dexter <dexter.lente18@gmail.com>
2024-08-22 12:15:08 +0800

    Sdads

--- src/app/api/marquee/route.ts
+++ src/app/api/marquee/route.ts
@@ -1,10 +1,23 @@
 import axios from "axios";
 
 export async function GET() {
-  const response = await axios.get(
-    `https://api.binance.com/api/v3/ticker/24hr?symbols=["BTCUSDT","ETHUSDT","LTCUSDT","DOGEUSDT","SOLUSDT","XRPUSDT","BNBUSDT","MATICUSDT","ADAUSDT","DOTUSDT","AVAXUSDT","LINKUSDT","UNIUSDT","TRXUSDT"]`
-  );
-  const data = await response.data;
+  try {
+    const response = await axios.get(
+      `https://api.binance.com/api/v3/ticker/24hr?symbols=["BTCUSDT","ETHUSDT","LTCUSDT","DOGEUSDT","SOLUSDT","XRPUSDT","BNBUSDT","MATICUSDT","ADAUSDT","DOTUSDT","AVAXUSDT","LINKUSDT","UNIUSDT","TRXUSDT"]`
+    );
+    const data = await response.data;
 
-  return Response.json({ data });
+    return new Response(JSON.stringify({ data }), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+
+    // Return an error response
+    return new Response(JSON.stringify({ error: "Failed to fetch data" }), {
+      headers: { "Content-Type": "application/json" },
+      status: 500,
+    });
+  }
 }
