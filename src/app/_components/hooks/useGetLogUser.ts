a3f821d676ce4000e4a454a60b3f97c89292523d
Dexter <dexter.lente18@gmail.com>
2024-08-22 15:01:21 +0800

    idk

--- src/app/_components/hooks/useGetLogUser.ts
+++ src/app/_components/hooks/useGetLogUser.ts
@@ -5,6 +5,7 @@ import Cookies from "js-cookie";
 export const useGetLogUser = () => {
   const token = Cookies.get("token");
   const url = `/api/profile/`;
+
   const { data, isLoading } = useSWR(token ? url : null, fetcher);
 
   return { data, isLoading };
